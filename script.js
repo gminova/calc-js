bd2e7e38e19735ba31326319a8bdc79d061530c5
gminova <minova1995@gmail.com>
2019-06-30 22:01:13 +0100

    equals comment made by mistake, fixed

--- script.js
+++ script.js
@@ -213,7 +213,7 @@ function equals() {
     result.value = eval(display.value);
     chunk = '';
     lockALLDigits();
-    //operations.decimal.disabled = true;
+    operations.decimal.disabled = true;
 }
 
 operations.equals.addEventListener('click', equals);
